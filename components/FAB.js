0095ef39b550e42f9de3bf2dff4e418a7f256714
kiruthiharan <kiruthiharan@gmail.com>
2020-06-16 21:08:54 +0530

    list styling and swipe buttons

---
+++ components/FAB.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {TouchableOpacity, StyleSheet} from 'react-native';
+import Colors from '../constants/Colors';
+
+const FAB = props => {
+  return (
+    <TouchableOpacity style={styles.TouchableOpacityStyle} {...props} >
+      <Icon name="plus" color="white" size={30}/>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  TouchableOpacityStyle: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 30,
+    borderRadius: 30,
+    borderWidth:1,
+    borderColor:'rgba(0,0,0,0.2)',
+    backgroundColor: Colors.primaryColor
+  },
+  FABStyle: {
+    
+  },
+});
+
+export default FAB;
--- screens/ListsScreen.js
+++ screens/ListsScreen.js
@@ -1,49 +1,124 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  FlatList,
+  Alert,
+} from 'react-native';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import FAB from '../components/FAB';
 
 import {LISTS} from '../data/dummy-data';
+import {Colors} from '../constants/Colors';
+import LinearGradient from 'react-native-linear-gradient';
+import Swipeable from 'react-native-swipeable-row';
+import Icon from 'react-native-vector-icons/Feather';
 
 function ListsScreen(props) {
+  const leftContent = <Text>Pull to activate</Text>;
+
+  const rightButtons = [
+    <TouchableHighlight style={styles.slideIcon}>
+      <Icon name="delete" size={25}/>
+    </TouchableHighlight>,
+    <TouchableHighlight style={styles.slideIcon}>
+      <Icon name="edit" size={25}/>
+    </TouchableHighlight>,
+  ];
+
   const renderGridItem = itemData => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
+      <Swipeable
+        leftContent={leftContent}
+        rightButtons={rightButtons}>
+        <TouchableOpacity
+        activeOpacity ={0.7}
         onPress={() => {
           props.navigation.navigate({
             routeName: 'ListDetail',
-            params: {listId: itemData.item.id},
+            params: {
+              listId: itemData.item.id
+            }
           });
-        }}>
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+        }}
+          >
+          <LinearGradient
+            colors={['#39A1F7', '#718BFB', '#A875FF']}
+            style={styles.linearGradient}>
+            <View>
+              <Text style={styles.title}>{itemData.item.title}</Text>
+              <Text style={styles.footer}>{itemData.item.title}</Text>
+            </View>
+          </LinearGradient>
+        </TouchableOpacity>
+      </Swipeable>
     );
   };
 
-  return <FlatList data={LISTS} renderItem={renderGridItem} numColumns={2} />;
+  return (
+    <View style={styles.root}>
+      <FlatList data={LISTS} renderItem={renderGridItem} numColumns={1} />
+      <FAB
+        onPress={() => {
+          Alert.alert('Hello');
+        }}
+      />
+    </View>
+  );
 }
 
 // optional for like setting heeaders
-ListsScreen.navigationOptions = (navigationData) => {
+ListsScreen.navigationOptions = navigationData => {
   return {
     headerTitle: 'Shopping Lists',
-    headerLeft:() => (
+    headerLeft: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title="Fav" iconName="navicon" onPress={() => {navigationData.navigation.toggleDrawer()}} />
+        <Item
+          title="Fav"
+          iconName="navicon"
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        />
       </HeaderButtons>
     ),
   };
 };
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   gridItem: {
     flex: 1,
+  },
+  linearGradient: {
+    flex: 1,
+    borderRadius: 5,
+    elevation: 5,
     margin: 15,
     height: 100,
+    marginHorizontal: 20
   },
+  title: {
+    fontWeight: 'bold',
+    color: 'white',
+    margin: 10,
+  },
+  footer: {
+    top: 30,
+    paddingRight: 10,
+    color: 'white',
+    alignSelf: 'flex-end',
+  },
+  slideIcon: {
+    top: 50,
+    
+  }
 });
 
 export default ListsScreen;
