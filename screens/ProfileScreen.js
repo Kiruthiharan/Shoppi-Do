07e4e119935e86b1860b8f856c9a481ce1aed5bb
kiruthiharan <kiruthiharan@gmail.com>
2020-06-25 23:32:59 +0530

    profile inouts

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -6,21 +6,32 @@ import {
   Text,
   StyleSheet,
   Image,
+  TextInput,
 } from 'react-native';
+import Colors from '../constants/Colors';
 
 function ProfileScreen(props) {
   return (
     <View style={styles.root}>
-      <Image
-        source={require('../assets/images/profile-default.jpg')}
-        style={styles.image}
-      />
+      <View>
+        <Image
+          source={require('../assets/images/profile-default.jpg')}
+          style={styles.image}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="UUserName" style={styles.input} />
+        <TextInput placeholder="UUserName" style={styles.input} />
+        <TextInput placeholder="UUserName" style={styles.input} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   root: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -29,7 +40,18 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 150 / 2,
     overflow: 'hidden',
-    margin: 10
+    margin: 10,
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    width: 300,
+    borderColor: Colors.primaryColor,
+    borderWidth: 1,
   },
 });
 
