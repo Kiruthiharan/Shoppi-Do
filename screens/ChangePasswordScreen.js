79197e901298943aa8dbbf33164fcf49f35097ff
kiruthiharan <kiruthiharan@gmail.com>
2020-07-27 23:03:31 +0530

    change password ui

--- screens/ChangePasswordScreen.js
+++ screens/ChangePasswordScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 const ChangePasswordScreen = props => {
   return (
-    <View>
+    <View style={styles.root}>
       <Text>Change Password</Text>
+      <TextInput style={styles.input}></TextInput>
+      <TextInput style={styles.input}></TextInput>
+      <TextInput style={styles.input}></TextInput>
+      <Button>Change Passord</Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    margin: 10
+  }
+})
 
 export default ChangePasswordScreen;
